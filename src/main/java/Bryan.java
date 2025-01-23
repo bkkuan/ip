bf41286f737d06d7200046a531f49e9552189c90
bkkuan <e1139171@u.nus.edu>
2025-01-24 06:29:51 +0800

    Implement Level-1: Echo functionality

--- src/main/java/Bryan.java
+++ src/main/java/Bryan.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Bryan {
     public static void main(String[] args) {
 
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Bryan");
+        System.out.println("Hello! I'm Bryan, your trustworthy support");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        String input;
+        while(true) {
+            input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            //echo user input
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        }
+        sc.close();
+
     }
 }
