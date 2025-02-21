9084522b134065144f088c608b14e6e0100f2186
bkkuan <e1139171@u.nus.edu>
2025-02-22 01:55:17 +0800

    fix: remove broken submodule reference

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Bryan");
             stage.setScene(scene);
             // Inject the Bryan instance into the controller.
             fxmlLoader.<seedu.bryan.MainWindow>getController().setBryan(bryan);
